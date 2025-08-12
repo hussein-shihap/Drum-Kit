33b909fe8a88b3cb8906b7a62700890ccd47ba5e
Hussein Shihab <H.Shihab1@outlook.com>
2025-08-12 18:23:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,40 +23,40 @@ playSound(buttonInnerHTML)
 function playSound(key){
 
 if(key==="w"){
-    let audio=new Audio("./sounds/tom-1.mp3");
+    let audio=new Audio("./tom-1.mp3");
     audio.play();
     }
 
 else if(key==="a"){
-    let audio=new Audio("./sounds/tom-2.mp3");
+    let audio=new Audio("./tom-2.mp3");
     audio.play();
     }
     
 else if(key==="s"){
-    let audio=new Audio("./sounds/tom-3.mp3");
+    let audio=new Audio("./tom-3.mp3");
     audio.play();
     }
     
     
 else if(key==="d"){
-    let audio=new Audio("./sounds/tom-4.mp3");
+    let audio=new Audio("./tom-4.mp3");
     audio.play();
     }
     
 else if(key==="j"){
-    let audio=new Audio("./sounds/snare.mp3");
+    let audio=new Audio("./snare.mp3");
     audio.play();
     }
     
     
 else if(key==="k"){
-    let audio=new Audio("./sounds/kick-bass.mp3");
+    let audio=new Audio("./kick-bass.mp3");
     audio.play();
     }
 
 
 else if(key==="l"){
-    let audio=new Audio("./sounds/crash.mp3");
+    let audio=new Audio("./crash.mp3");
     audio.play();
     }
 
@@ -66,4 +66,5 @@ else if(key==="l"){
 
 document.addEventListener("keydown", function(event) {
   playSound(event.key);
+
 });
