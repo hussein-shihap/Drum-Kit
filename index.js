ca40e90203cfb8bdfb350538f3d7a84149547a0a
Hussein Shihab <H.Shihab1@outlook.com>
2025-08-19 12:20:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,84 @@
 
 
 
-
-
 const buttons= document.querySelectorAll(".drum");
 
-for(let i=0;i<button1.length;i++){
-
-    button1[i].addEventListener("click",  function(){
+for(let i=0; i<buttons.length;i++){
 
 
-let buttonInnerHTML=this.innerHTML;
-playSound(buttonInnerHTML)
-          addAnimation(buttonInnerHTML);
+buttons[i].addEventListener("click", function(){
 
 
+let buttonInner=this.innerHTML;
+playSound(buttonInner);
+addAnimation(buttonInner);
 
-    } );
-  
-  }
+});
+
+
+}
+
+
 
 
 function playSound(key){
 
 if(key==="w"){
     let audio=new Audio("./tom-1.mp3");
-    audio.play();
-    }
+     audio.play();
 
-else if(key==="a"){
-    let audio=new Audio("./tom-2.mp3");
-    audio.play();
-    }
-    
-else if(key==="s"){
-    let audio=new Audio("./tom-3.mp3");
-    audio.play();
-    }
-    
-    
-else if(key==="d"){
-    let audio=new Audio("./tom-4.mp3");
-    audio.play();
-    }
-    
-else if(key==="j"){
-    let audio=new Audio("./snare.mp3");
-    audio.play();
-    }
-    
-    
-else if(key==="k"){
-    let audio=new Audio("./kick-bass.mp3");
-    audio.play();
-    }
-
-
-else if(key==="l"){
-    let audio=new Audio("./crash.mp3");
-    audio.play();
-    }
 
 
 }
 
+else if(key==="a"){
+      let audio=new Audio("./tom-2.mp3");
+     audio.play();
 
-document.addEventListener("keydown", function(event) {
-  playSound(event.key);
-    addAnimation(event.key);
+
+
+}
+
+else if(key==="s"){
+      let audio=new Audio("./tom-3.mp3");
+     audio.play();
+
+}
+
+
+else if(key==="d"){
+      let audio=new Audio("./tom-4.mp3");
+     audio.play();}
+     
+ else if(key==="j"){
+      let audio=new Audio("./snare.mp3");
+     audio.play();}
+     
+ else if(key==="k"){ 
+      let audio=new Audio("./kick-bass.mp3");
+     audio.play();}
+     
+ else if(key==="l"){
+      let audio=new Audio("./crash.mp3");
+    audio.play();
+    }
+
+ else {
+
+    console.log("inavalid key pressesd")
+ }     
+}
+
+
+
+document.addEventListener("keydown", function(event){
+
+playSound(event.key)
+addAnimation(event.key)
 
 });
+
+
 
 
 function addAnimation(currentKey) {
